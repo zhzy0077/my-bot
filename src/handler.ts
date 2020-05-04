508bcf87bca57cf1ddb027c21e1fce537ded5c65
Zhiyuan Zheng <zhiyuan.zheng@yandex.com>
2020-05-04 18:21:45 +0800

    Update.

--- src/handler.ts
+++ src/handler.ts
@@ -1,118 +1,134 @@
-import { Octokit } from "@octokit/rest";
-import { Secrets } from "./secrets";
-import { createTransport } from 'nodemailer'
+import { Octokit } from '@octokit/rest'
+import { Secrets } from './secrets'
 
 const HEADERS: HeadersInit = {
-  "Content-Type": "application/json",
-  "Accept": "application/json",
+  'Content-Type': 'application/json',
 }
 
 const octokit = new Octokit({
   auth: Secrets.GIST_TOKEN,
-});
+})
 
-const AccessTokenFile = "access_token";
+const AccessTokenFile = 'access_token'
+const FallbackEvent = 'fallback'
+const toUser = 'org6fvzbNIm5TlJTCShbKFyd59UA'
 
-export async function handleRequest(request: Request): Promise<Response> {
-  const message = await request.text();
+export async function handleRequest (request: Request): Promise<Response> {
+  const message = await request.text()
+  const context = await getContext()
   try {
-    const accessToken = await getAccessToken();
-    const result = await sendMessage(message, accessToken);
-    await log(`Request {${JSON.stringify(request)}} Response: {${JSON.stringify(result)}}`);
-    return new Response(JSON.stringify(result));
+    const result = await sendMessage(message, context)
+    await log(`Request {${JSON.stringify(request)}} Response: {${JSON.stringify(result)}}`, context)
+    return new Response(JSON.stringify(result))
   } catch (err) {
-    await sendEmail(`Fallback message from bot: ${JSON.stringify(err)}.`, message);
-    await log(JSON.stringify(err));
-    return new Response(err.stack || err)
+    const errorMsg = JSON.stringify(err)
+    await fallback(errorMsg, message)
+    await log(errorMsg, context)
+    return new Response(errorMsg)
+  } finally {
+    await saveContext(context)
   }
 }
 
-async function getAccessToken(): Promise<string> {
-  const cacheToken = await readAccessTokenFromCache();
-  if (now() < cacheToken.expires_at) {
-    return cacheToken.access_token;
+async function getContext (): Promise<Context> {
+  const context = await readContext()
+  if (now() >= context.weChatAccessToken.expires_at) {
+    context.weChatAccessToken = await fetchAccessTokenFromOrigin()
   }
-  const latestToken = await fetchAccessTokenFromOrigin();
-  await updateAccessTokenCache(latestToken);
-  return latestToken.access_token;
+
+  return context
 }
 
-async function readAccessTokenFromCache(): Promise<WeChatAccessToken> {
+async function readContext (): Promise<Context> {
   const { data } = await octokit.gists.get({
     gist_id: Secrets.GIST_ID,
-  });
-  const content = (data.files as any)[AccessTokenFile].content;
-  return JSON.parse(content) as WeChatAccessToken;
+  })
+  const accessToken = (data.files as any)[AccessTokenFile]
+  const logUrl = (data.files as any)[today()]
+  return <Context>{
+    weChatAccessToken: JSON.parse(accessToken.content) as WeChatAccessToken,
+    logUrl: logUrl?.raw_url as string,
+  }
 }
 
-async function updateAccessTokenCache(token: WeChatAccessToken): Promise<void> {
+async function fallback (errMsg: string, message: string): Promise<void> {
+  const url = `https://maker.ifttt.com/trigger/${FallbackEvent}/with/key/${Secrets.IFTTT_KEY}`
+  const fallbackMsg = {
+    value1: message,
+    value2: errMsg,
+  }
+  await fetch(url, {
+    method: 'POST',
+    headers: HEADERS,
+    body: JSON.stringify(fallbackMsg)
+  })
+}
+
+async function saveContext (context: Context): Promise<void> {
+  let logContent = context.newLog
+  if (context.logUrl) {
+    logContent = await (await fetch(context.logUrl)).text() + logContent
+  }
+
   await octokit.gists.update({
     gist_id: Secrets.GIST_ID,
     files: {
       [AccessTokenFile]: {
-        content: JSON.stringify(token),
+        content: JSON.stringify(context.weChatAccessToken),
+      },
+      [today()]: {
+        content: logContent,
       }
     } as any
-  });
+  })
 }
 
-async function log(log: string): Promise<void> {
-  await octokit.gists.createComment({
-    gist_id: Secrets.GIST_ID,
-    body: log,
-  });
+async function log (log: string, context: Context): Promise<void> {
+  context.newLog += `${log}\n`
 }
 
-async function sendEmail(title: string, message: string): Promise<void> {
-  const transport = createTransport({
-    host : "smtp.aliyun.com",
-    port: 465,
-    secure: true,
-    auth : {
-      user : Secrets.EMAIL_ADDRESS,
-      pass : Secrets.EMAIL_PASSWORD,
-    },
-  });
-
-  await transport.sendMail({
-    from: Secrets.EMAIL_ADDRESS,
-    to: Secrets.EMAIL_ADDRESS,
-    subject: title,
-    text: message,
-  });
+async function fetchAccessTokenFromOrigin (): Promise<WeChatAccessToken> {
+  const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${Secrets.WX_APPID}&secret=${Secrets.WX_APPSECRET}`
+  const result = await fetchTyped<WeChatAccessToken>(url)
+  result.expires_at = now() + result.expires_in
+  return result
 }
 
-async function fetchAccessTokenFromOrigin(): Promise<WeChatAccessToken> {
-  const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${Secrets.WX_APPID}&secret=${Secrets.WX_APPSECRET}`;
-  const result = await fetchTyped<WeChatAccessToken>(url);
-  result.expires_at = now() + result.expires_in;
-  return result;
-}
-
-async function sendMessage(message: string, accessToken: string): Promise<WeChatSendMessageReply> {
-  const url = `https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=${accessToken}`;
+async function sendMessage (message: string, context: Context): Promise<WeChatSendMessageReply> {
+  var accessToken = context.weChatAccessToken.access_token
+  const url = `https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=${accessToken}`
   const wxMessage: WeChatMessage = {
-    touser: "org6fvzbNIm5TlJTCShbKFyd59UA",
-    msgtype: "text",
+    touser: toUser,
+    msgtype: 'text',
     text:
-    {
-      content: message,
-    },
-  };
+      {
+        content: message,
+      },
+  }
   return await fetchTyped<WeChatSendMessageReply>(url, {
-    method: "POST",
+    method: 'POST',
     headers: HEADERS,
     body: JSON.stringify(wxMessage)
-  });
+  })
 }
 
-async function fetchTyped<T>(input: RequestInfo, init?: RequestInit): Promise<T> {
-  const response = await fetch(input, init);
-  return response.json() as Promise<T>;
+async function fetchTyped<T> (input: RequestInfo, init?: RequestInit): Promise<T> {
+  const response = await fetch(input, init)
+  return await response.json() as Promise<T>
 }
 
-function now(): number {
+function now (): number {
   return new Date().getTime() / 1000
+}
+
+function today (): string {
+  return new Date().toISOString().slice(0, 10)
+}
+
+interface Context {
+  weChatAccessToken: WeChatAccessToken,
+  logUrl: string,
+  newLog: string,
 }
 
 interface WeChatAccessToken {
@@ -133,3 +149,4 @@ interface WeChatSendMessageReply {
   errcode: number,
   errmsg: string,
 }
+
